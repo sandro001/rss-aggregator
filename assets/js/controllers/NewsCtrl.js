0efb1afe5079babc26aa7b6f2216a2470725cc01
Yevgeniya Bronfman <in.in.formatio@gmail.com>
2016-12-26 03:40:48 +0200

    added form for adding news, tags in sidebar

--- assets/js/controllers/NewsCtrl.js
+++ assets/js/controllers/NewsCtrl.js
@@ -1,6 +1,6 @@
 (function(){
+
   angular.module('App').controller('NewsCtrl', ['$scope', 'ApiService', function($scope, ApiService) {
-    console.log('AAAAAAAAAAA')
     ApiService.getArticles(null, function(err, res) {
       $scope.news = {
         0: {
