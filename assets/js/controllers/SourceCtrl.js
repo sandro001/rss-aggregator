bc2bc690f0e74875253028b2e833c75ee119cee2
Yevgeniya Bronfman <in.in.formatio@gmail.com>
2016-12-27 20:10:58 +0200

    added new styles for sources and news

--- assets/js/controllers/SourceCtrl.js
+++ assets/js/controllers/SourceCtrl.js
@@ -2,7 +2,7 @@
 	var app = angular.module('App');
 
 	app.controller('AddSourceCtrl', ['$scope', '$state', 'UserService', 'ApiService',
-		
+
 	function($scope, $state, UserService, ApiService) {
 		$scope.ctr = {
 			sourceName: '',
@@ -10,7 +10,7 @@
 
 			addSource: addSource
 		}
-	    
+
 
 	    function addSource() {
 	    	if(!$scope.ctr.sourceName || !$scope.ctr.sourceUrl) return;
@@ -23,17 +23,17 @@
 	    }
 	}])
 
-	app.controller('SourcesCtrl', ['$scope', '$state', 'ApiService', 
-		
+	app.controller('SourcesCtrl', ['$scope', '$state', 'ApiService',
+
 	function($scope, $state, ApiService) {
 		$scope.ctr = {
-			sources: [],
+			sources: [{"url":"http://www.pravda.com.ua/rss/","name":"pravda all","id":1,"createdAt":"2016-12-25T22:15:42.000Z","updatedAt":"2016-12-25T22:15:42.000Z","mySubscr":false},{"url":"http://football.ua/rss2.ashx","name":"футбол","id":2,"createdAt":"2016-12-26T20:16:54.000Z","updatedAt":"2016-12-26T20:16:54.000Z","mySubscr":false}],
 			reqObj: {
 			},
 
 			subscribe: subscribe
 		}
-	    
+
 		ApiService.getSources(null, function(err, sources) {
 			$scope.ctr.sources = sources.results;
 		})
@@ -45,6 +45,6 @@
 				source.mySubscr = true;
 			})
 		}
-	    
+
 	}])
 })();
